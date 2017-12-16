a87a86cab786fda4d47ae284c580bb43664d1b00
Rittick <drittick@gmail.com>
2017-12-15 21:53:56 -0800

    Java Conditionals

---
+++ Java Refresher/src/rittick/code/classes/Decisions.java
@@ -0,0 +1,15 @@
+package rittick.code.classes;
+
+public class Decisions {
+	public static void main(String[] args) {
+		int a = -30;
+		
+		if(a > 0){
+			System.out.println("a is greater than 0.");
+		}else if (a<0){
+			System.out.println("a is less than 0.");
+		}else {
+			System.out.println("I don't care about a");
+		}
+	}
+}
