ee4538cf95bd3b1d021cca683a07b92262850f7d
Rittick <drittick@gmail.com>
2017-12-15 21:24:44 -0800

    Java Variables and Operations

---
+++ Java Refresher/src/rittick/code/classes/Person.java
@@ -0,0 +1,12 @@
+package rittick.code.classes;
+
+public class Person {
+	private static double salary;
+	
+	private static final String dept = "Department";
+	
+	public static void main(String[] args){
+		salary = 100;
+		System.out.println("Salary: "+salary);
+	}
+}
---
+++ Java Refresher/src/rittick/code/classes/arithmetic.java
@@ -0,0 +1,15 @@
+package rittick.code.classes;
+
+/**
+ * @author Rittick
+ *
+ */
+public class arithmetic {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		int c = (a<b)?30:40;
+		System.out.println("Value of c is: "+c);
+	}
+}
